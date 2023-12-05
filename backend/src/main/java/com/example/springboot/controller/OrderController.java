132c52205dd7ca3c38a7079527ae52c43a2f3470
sudhakar75 <73861716+sudhakar75@users.noreply.github.com>
2023-12-05 13:57:05 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/springboot/controller/OrderController.java
@@ -0,0 +1,72 @@
+package com.example.springboot.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.springboot.model.Order;
+import com.example.springboot.service.OrderService;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+
+@RestController 
+@RequestMapping("/api/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService orderService;
+
+	
+
+//adding order details
+	@PostMapping("{customerId}/{cartId}")
+	public ResponseEntity<Order> addOrder( @PathVariable long customerId,@PathVariable long cartId, @RequestBody Order order) {
+
+		return new ResponseEntity<Order>(orderService.addOrder(order,customerId,cartId),
+				HttpStatus.CREATED);
+	}
+
+
+	@PutMapping("{orderId}")
+	public ResponseEntity<Order> updateOrder(@PathVariable("orderId") long orderId, @RequestBody Order order) {
+		return new ResponseEntity<Order>(orderService.updateOrder(order, orderId), HttpStatus.OK);
+	}
+
+	
+	@GetMapping()
+	public List<Order> getAllOrders() {
+
+		return orderService.getAllOrders();
+	}
+
+	
+	@GetMapping("{customerId}")
+	public List<Order> getOrderByCustomerId(@PathVariable long customerId) {
+		return orderService.getOrderByCustomerId(customerId);
+	}
+
+
+
+	@DeleteMapping("{orderId}")
+	public ResponseEntity<Boolean> deleteBooking(@PathVariable("orderId") long orderId) {
+		orderService.deleteOrder(orderId);
+		boolean flag = true;
+		return new ResponseEntity<Boolean>(flag, HttpStatus.OK);
+	}
+	@PostMapping("/addOrder/{customerId}")
+	public ResponseEntity<Order> addOrderItems( @PathVariable long customerId, @RequestBody Order order) {
+		return new ResponseEntity<Order>(orderService.addOrderItem(order,customerId),
+				HttpStatus.CREATED);
+	}
+}
